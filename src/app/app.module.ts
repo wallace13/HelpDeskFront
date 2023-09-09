43b6404789a57cdca45b8fe5d14446dfb3e38d62
wallace13 <wsalesg@hotmail.com>
2023-09-09 00:05:05 -0300

    Create Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,12 +21,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
