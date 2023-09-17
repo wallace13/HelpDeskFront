22c5a5af002b1f076eb1e16dd1fecae6ca652e2d
wallace13 <wsalesg@hotmail.com>
2023-09-17 16:23:52 -0300

    Crud Chamado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,10 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -26,7 +30,12 @@ const routes: Routes = [
       { path: 'clientes', component: ClienteListComponent },
       { path: 'clientes/create', component: ClienteCreateComponent },
       { path: 'clientes/update/:id', component: ClienteUpdateComponent },
-      { path: 'clientes/delete/:id', component: ClienteDeleteComponent }
+      { path: 'clientes/delete/:id', component: ClienteDeleteComponent },
+
+      { path: 'chamados', component:   ChamadoListComponent },
+      { path: 'chamados/create', component:   ChamadoCreateComponent },
+      { path: 'chamados/update/:id', component:   ChamadoUpdateComponent },
+      { path: 'chamados/read/:id', component:     ChamadoReadComponent },
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,10 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 //import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
@@ -55,6 +59,10 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
     ClienteCreateComponent,
     ClienteUpdateComponent,
     ClienteDeleteComponent,
+    ChamadoListComponent,
+    ChamadoCreateComponent,
+    ChamadoUpdateComponent,
+    ChamadoReadComponent,
   ],
   imports: [
     BrowserModule,
